341f0f07c2a1f3ec9f14d6d30728bb86b544ed66
mayankb5 <mayank.bhatia@kellogg.northwestern.edu>
2022-03-16 20:30:44 +0000

    Debug 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
-        flash[:notice] = "Welcome, #{@user.first_name}."
-        redirect_to "/companies"
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
       else
         flash[:notice] = "Email or password not recognized"
         redirect_to "/sessions/new"
